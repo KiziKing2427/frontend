dba60556ff059a852349ed4c285b524cb6b0805a
kiziKing2427 <ugofabian7@gmail.com>
2024-04-11 16:05:40 +0700

    Deploy frontend to Herok7

--- src/api/axios.jsx
+++ src/api/axios.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'https://your-react-frontend.herokuapp.com'
+    baseURL: 'https://ytrifrontend-516983977383.herokuapp.com/'
 });
